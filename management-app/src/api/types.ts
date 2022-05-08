c4ced7b9969c42fc20cdcb862c4b6de2792cc967
MarinaPresmytskaia <73139136+MarinaPresmytskaia@users.noreply.github.com>
2022-05-08 20:18:40 +0300

    Create api layer (#3)
    * fix: fix error
    
    * feat: add config for HTTP status codes
    
    * feat: add env
    
    * feat: add baseAPI
    
    * feat: add userAPI and errors
    
    * refactor: add methods to userAPI
    
    * refactor: typo fix
    
    * refactor: rewrite userAPI

---
+++ management-app/src/api/types.ts
@@ -0,0 +1,12 @@
+export type TAPIErrorsMap = {
+  [key: number]: string;
+};
+
+export type TUserBase = {
+  login: string;
+  password: string;
+};
+
+export type TUser = TUserBase & {
+  name: string;
+};
---
+++ management-app/src/api/usersAPI.ts
@@ -0,0 +1,57 @@
+/* eslint-disable no-console */
+import { BaseAPI } from './baseAPI';
+import { USERS_API_ERRORS } from './errors';
+import { TUser, TUserBase } from './types';
+
+class UsersAPI extends BaseAPI {
+  updateUser(userId: string, token: string, user: TUser): Promise<TUser> {
+    return this.put(`users/${userId}`, user, {
+      Authorization: `Bearer ${token}`,
+    }).then((result) => {
+      BaseAPI.handleError(result, USERS_API_ERRORS);
+      return result.json();
+    });
+  }
+
+  getUser(userId: string, token: string): Promise<TUser> {
+    return this.get(`users/${userId}`, {
+      Authorization: `Bearer ${token}`,
+    }).then((result) => {
+      BaseAPI.handleError(result, USERS_API_ERRORS);
+      return result.json();
+    });
+  }
+
+  getUsers(token: string): Promise<TUser[]> {
+    return this.get('users', {
+      Authorization: `Bearer ${token}`,
+    }).then((result) => {
+      BaseAPI.handleError(result, USERS_API_ERRORS);
+      return result.json();
+    });
+  }
+
+  deleteUser(userId: string, token: string): Promise<void> {
+    return this.delete(`users/${userId}`, {
+      Authorization: `Bearer ${token}`,
+    }).then((result) => {
+      BaseAPI.handleError(result, USERS_API_ERRORS);
+    });
+  }
+
+  createToken(dataBase: TUserBase): Promise<TUserBase> {
+    return this.post('signin', dataBase).then((result) => {
+      BaseAPI.handleError(result, USERS_API_ERRORS);
+      return result.json();
+    });
+  }
+
+  createAccount(dataUser: TUser): Promise<TUser> {
+    return this.post('signup', dataUser).then((result) => {
+      BaseAPI.handleError(result, USERS_API_ERRORS);
+      return result.json();
+    });
+  }
+}
+
+export default new UsersAPI();
