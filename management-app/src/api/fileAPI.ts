f7b906018a704cd8d0afcd869b8af703e57a6bb9
MarinaPresmytskaia <mpresmytskaia@gmail.com>
2022-05-09 23:33:26 +0300

    fix: fix typo

--- management-app/src/api/fileAPI.ts
+++ management-app/src/api/fileAPI.ts
@@ -3,14 +3,16 @@ import { FILE_API_ERRORS, USERS_API_ERRORS } from './errors';
 import { TFile } from './types';
 
 class FileAPI extends BaseAPI {
+  //  @todo check during implementing uploadFile logic
   uploadFile(token: string, file: TFile): Promise<void> {
     return this.post(`file`, file, {
       Authorization: `Bearer ${token}`,
     }).then((result) => {
-      BaseAPI.handleError(result, USERS_API_ERRORS);
+      BaseAPI.handleError(result, FILE_API_ERRORS);
       return result.json();
     });
   }
+  //  @todo check during implementing downloadFile logic
 
   downloadFile(token: string, taskId: string, fileName: string): Promise<void> {
     return this.get(`file/${taskId}/${fileName}`, {
