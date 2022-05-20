b774a60f51a61ff4a82e2119a300e9c605ac802c
MarinaPresmytskaia <mpresmytskaia@gmail.com>
2022-05-20 17:18:35 +0300

    refactor: rewrite order into taskCard

--- management-app/src/pages/BoardPage/components/TaskCard/TaskCard.tsx
+++ management-app/src/pages/BoardPage/components/TaskCard/TaskCard.tsx
@@ -4,40 +4,50 @@ import { Box, Typography } from '@mui/material';
 import { updateTask } from '../../boardSlice';
 
 import { TTaskCardProps } from './TaskCard.types';
-import { useAppDispatch } from '../../../../store';
+import { useAppDispatch, useAppSelector } from '../../../../store';
+import { TColumnResponse } from '../../../../api/types';
 
 // TODO: use TColumn instead of BoardColumnProps?
 export function TaskCard({ taskInfo, columnId, boardId }: TTaskCardProps) {
   const { title, id, description, order, userId } = taskInfo;
+  const { columns } = useAppSelector((state) => state.board.boardData);
 
   const dispatch = useAppDispatch();
 
-  //  TODO: use right order
-  const [, drag] = useDrag(() => ({
-    type: 'taskCard',
-    item: { id, title, description, order, userId },
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-    end: (item, monitor) => {
-      const fineshColumnId = monitor.getDropResult() as { id: string };
-      dispatch(
-        updateTask({
-          boardId,
-          columnId,
-          taskId: id,
-          task: {
-            title,
-            order,
-            description,
-            userId,
+  const [, drag] = useDrag(
+    () => ({
+      type: 'taskCard',
+      item: { id, title, description, order, userId },
+      collect: (monitor) => ({
+        isDragging: !!monitor.isDragging(),
+      }),
+      end: (item, monitor) => {
+        const finishColumnId = monitor.getDropResult() as { id: string };
+
+        const finishColumn = columns.find(
+          (column) => column.id === finishColumnId.id
+        ) as TColumnResponse;
+
+        const taskOrder = finishColumn.tasks.length + 1;
+        dispatch(
+          updateTask({
             boardId,
-            columnId: fineshColumnId?.id,
-          },
-        })
-      );
-    },
-  }));
+            columnId,
+            taskId: id,
+            task: {
+              title,
+              order: taskOrder,
+              description,
+              userId,
+              boardId,
+              columnId: finishColumnId?.id,
+            },
+          })
+        );
+      },
+    }),
+    [columns]
+  );
   return (
     <Box
       ref={drag}
