40270bca38482530588ddd8d562c88a0319843e9
elenaBezro <lenyka_86@mail.ru>
2022-05-20 11:56:17 +0200

    fix: fix dispatch type error

--- management-app/src/components/FormField/FormField.types.ts
+++ management-app/src/components/FormField/FormField.types.ts
@@ -4,6 +4,6 @@ export type TFormFieldProps = {
   placeholder: string;
   fieldName: string;
   taskTitle: string;
-  onChange: Dispatch<string, string>;
+  onChange: (value: string, fieldName: string) => void;
   isMultiline?: boolean;
 };
