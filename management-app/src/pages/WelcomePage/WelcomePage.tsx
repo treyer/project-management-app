328a8bafbfc6725bf39e241bb699cc46126eb9db
MarinaPresmytskaia <mpresmytskaia@gmail.com>
2022-05-22 16:18:58 +0300

    refactor: fix routing

--- management-app/src/pages/WelcomePage/WelcomePage.tsx
+++ management-app/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,22 +1,11 @@
 import { Card, CardMedia, CardContent, Typography } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../../store';
+
 import TeamInfoCard from './components/TeamInfoCard';
 import { TEAM_INFO } from './constants';
 
 import style from './WelcomePage.module.css';
 
 function WelcomePage() {
-  const { isLoggedIn } = useAppSelector((state) => state.auth);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate('/main');
-    }
-  }, [isLoggedIn, navigate]);
-
   return (
     <div className={style.main}>
       <div className={style.sectionStart}>
